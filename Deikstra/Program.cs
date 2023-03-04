558e92275abe652c410e8213c16d612599bab46c
Tintana <miiagkov123456@gmail.com>
2023-03-04 10:45:40 +0600

    Adding dijkstra and golden fish

---
+++ Deikstra/Program.cs
@@ -0,0 +1,91 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+DataTable _dt = new DataTable();
+_dt = ImportSheet("");
+Dijkstra(_dt,0,9);
+static DataTable ImportSheet(string fileName)
+{
+    var datatable = new DataTable();
+    var workbook = new XLWorkbook(fileName);
+    var xlWorksheet = workbook.Worksheet(1);
+    var range = xlWorksheet.Range(xlWorksheet.FirstCellUsed(), xlWorksheet.LastCellUsed());
+
+    var col = range.ColumnCount();
+    var row = range.RowCount();
+
+    //if a datatable already exists, clear the existing table 
+    datatable.Clear();
+    for (var i = 1; i <= col; i++)
+    {
+        var column = xlWorksheet.Cell(1, i);
+        datatable.Columns.Add(column.Value.ToString());
+    }
+
+    var firstHeadRow = 0;
+    foreach (var item in range.Rows())
+    {
+        if (firstHeadRow != 0)
+        {
+            var array = new object[col];
+            for (var y = 1; y <= col; y++)
+            {
+                array[y - 1] = item.Cell(y).Value;
+            }
+
+            datatable.Rows.Add(array);
+        }
+        firstHeadRow++;
+    }
+    return datatable;
+}
+
+int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+{
+    int min = int.MaxValue;
+    int minIndex = 0;
+
+    for (int v = 0; v < verticesCount; ++v)
+    {
+        if (shortestPathTreeSet[v] == false && distance[v] <= min)
+        {
+            min = distance[v];
+            minIndex = v;
+        }
+    }
+
+    return minIndex;
+}
+
+void Print(int[] distance, int verticesCount)
+{
+    Console.WriteLine("Vertex    Distance from source");
+
+    for (int i = 0; i < verticesCount; ++i)
+        Console.WriteLine("{0}\t  {1}", i, distance[i]);
+}
+
+void Dijkstra(int[,] graph, int source, int verticesCount)
+{
+    int[] distance = new int[verticesCount];
+    bool[] shortestPathTreeSet = new bool[verticesCount];
+
+    for (int i = 0; i < verticesCount; ++i)
+    {
+        distance[i] = int.MaxValue;
+        shortestPathTreeSet[i] = false;
+    }
+
+    distance[source] = 0;
+
+    for (int count = 0; count < verticesCount - 1; ++count)
+    {
+        int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+        shortestPathTreeSet[u] = true;
+
+        for (int v = 0; v < verticesCount; ++v)
+            if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
+                distance[v] = distance[u] + graph[u, v];
+    }
+
+    Print(distance, verticesCount);
+}
---
+++ Golden fish/Goldenfish/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+var text = File.ReadAllLines(@"D:/Programming/Univer/file.txt");
+List<string> words = new List<string>();
+List<string> beginningsbukva = new List<string>();
+List<int> beginningscount = new List<int>();
+List<string> endings = new List<string>();
+List<int> endingscount = new List<int>();
+int countwords = Convert.ToInt32(text[0])+1;
+for(int i = 1; i<countwords; i++)
+{
+    words.Add(text[i]);
+    //Console.WriteLine(text[i]);
+}
+//Console.WriteLine(words.Count);
+int beginningcount = Convert.ToInt32(text[countwords])+1;
+//Console.WriteLine(beginningcount);
+for(int j = countwords+1; j<beginningcount+countwords; j++)
+{
+    var temp = text[j].Split().ToList();
+    //Console.WriteLine($"{temp[0]}, {temp[1]}");
+    beginningsbukva.Add(temp[0]);
+    beginningscount.Add(Convert.ToInt32(temp[1]));
+}
+int endingcount = Convert.ToInt32(text[beginningcount + countwords]) + 1;
+//Console.WriteLine(endingcount);
+for (int k = endingcount+countwords+1; k < text.Length; k++)
+{
+    var temp = text[k].Split().ToList();
+    endings.Add(temp[0]);
+    //Console.WriteLine($"{temp[0]},{temp[1]}");
+    endingscount.Add(Convert.ToInt32(temp[1]));
+}
+int count = 0;
+int flag = 0;
+int index = 0;
+//Console.WriteLine("------------------------------------");
+for (int i=1; i<words.Count; i++)
+{
+    string word = words[i];
+    Console.WriteLine(word);
+    for(int j = 0; j<beginningscount.Count; j++)
+    {
+        if (Convert.ToString(word[0]) == beginningsbukva[j])
+        {
+            beginningscount[j] = beginningscount[j] - 1;
+            flag++;
+            index = j;
+            Console.WriteLine($"flag: {flag}");
+            Console.WriteLine($"Beginning: {word[0]}, {beginningscount[j]}");
+            if (beginningscount[j] == 0)
+            {
+                beginningsbukva.Remove(beginningsbukva[j]);
+                beginningscount.Remove(beginningscount[j]);
+            }
+            break;
+        }
+    }
+    for(int k = 0; k<endingscount.Count; k++)
+    {
+        if ((Convert.ToString(word[^1]) == endings[k])&&(flag==1))
+        {
+            endingscount[k] = endingscount[k] - 1;
+            flag++;
+            Console.WriteLine($"flag: {flag}");
+            Console.WriteLine($"Ending: {word[^1]}, {endingscount[k]}");
+            if (endingscount[k] == 0)
+            {
+                endings.Remove(endings[k]);
+                endingscount.Remove(endingscount[k]);
+            }
+            break;
+        }
+    }
+    if(flag==2)
+    {
+        count++;
+        Console.WriteLine("DA");
+        index = 0;
+    }
+    else
+    {
+        beginningscount[index]++;
+        Console.WriteLine("NET");
+        index = 0;
+    }
+    flag = 0;
+    Console.WriteLine("-----------------------------------");
+}
+Console.WriteLine(count);
+/*
+ * Проверяем начало на буквы, находим букву, вычитаем единицу, ищем конец, находим, вычитаем единицу, прибавляем найденные
+ * слова. Проверяем на кончившиеся начальные и конечные буквы. Убираем в случае если их больше нет. Всё=)
+ */
